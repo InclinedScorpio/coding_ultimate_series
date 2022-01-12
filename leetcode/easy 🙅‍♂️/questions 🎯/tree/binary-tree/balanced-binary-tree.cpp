8af30168f4294d2d6343c20d050464aca07ac2a4
Ashutosh Tiwari <ashutoshtiwari3309@gmail.com>
2022-01-13 01:29:07 +0530

    Update binary tree balancing problem

--- leetcode/easy 🙅‍♂️/questions 🎯/tree/binary-tree/balanced-binary-tree.cpp
+++ leetcode/easy 🙅‍♂️/questions 🎯/tree/binary-tree/balanced-binary-tree.cpp
@@ -12,6 +12,15 @@
  * };
  */
 
+
+/**
+ * LOGIC: More than 1 thing can be returned from recursion.
+ * 
+ * - For base condition return back
+ * - There are generally 3 steps in recursion - base condition, next call, some calculation and return
+ * 
+ * 
+ * /
 struct TreeData {
     int height;
     bool isBalanced;
@@ -21,15 +30,21 @@ class Solution {
 public:
     
     TreeData iterateAndCheck(TreeNode* root) {
+	    // null must return height 0 and it will be obviously balanced on leaf node so {0, true}
         if(root==NULL) {
             struct TreeData rootCase = {0, true};
             return rootCase;
         }
+	    // keep calling left and right nodes & return struct which must carry height and isBalanced
         TreeData leftHeight = iterateAndCheck(root->left);
         TreeData rightHeight = iterateAndCheck(root->right);
+		
+		// if left or right gives isBalanced as false - just return back (no one cares about int in this problem so kept 0)
         if(!leftHeight.isBalanced || !rightHeight.isBalanced) {
             return {0, false};
         }
+		
+		// if reached here, evaluate the height and if height > 1
         struct TreeData newTreeData;
         newTreeData.height = max(leftHeight.height, rightHeight.height) + 1;
         newTreeData.isBalanced = abs(leftHeight.height-rightHeight.height) > 1 ? false : true;
