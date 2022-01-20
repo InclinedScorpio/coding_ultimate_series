e912708cb56707fe2ef96e1f02aa52ab51ecfa4b
Vaibhav Gupta <vaibhavgupta305@gmail.com>
2022-01-21 00:32:57 +0530

    array-Day1-DS

---
+++ data-structures/arrays/find_Kth_smallest_number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of elements of array: ";
+    cin >> n;
+    int a[n];
+    cout << "Enter the elements of array: ";
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    cout << "Enter the Kth smaller value: ";
+    int k;
+    cin >> k;
+    sort(a, a + n);
+    cout << k << "th smallest element in array is: " << a[k - 1];
+    return 0;
+}
