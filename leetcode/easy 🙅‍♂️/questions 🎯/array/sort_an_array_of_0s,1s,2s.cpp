3c00c6f67d0e1b3c3b3267eebf44717999439fa2
Vaibhav Gupta <vaibhavgupta305@gmail.com>
2022-01-21 00:17:08 +0530

    array-Day1-Easy

---
+++ leetcode/easy 🙅‍♂️/questions 🎯/array/sort_an_array_of_0s,1s,2s.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in array: ";
+    cin >> n;
+    cout << "Enter the elements of array: ";
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int zeros = 0;
+    int ones = 0;
+    int twos = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == 0)
+            zeros++;
+        if (a[i] == 1)
+            ones++;
+        if (a[i] == 2)
+            twos++;
+    }
+    if (zeros > 0)
+        for (int i = 0; i < zeros; i++)
+            cout << "0 ";
+    if (ones > 0)
+        for (int i = 0; i < ones; i++)
+            cout << "1 ";
+    if (twos > 0)
+        for (int i = 0; i < twos; i++)
+            cout << "2 ";
+    return 0;
+}
