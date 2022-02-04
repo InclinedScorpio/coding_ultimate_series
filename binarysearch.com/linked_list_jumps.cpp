029adffdb0f5ae780b95826a3c665c5692dd491d
Ashutosh Tiwari <ashutoshtiwari3309@gmail.com>
2022-02-05 00:14:32 +0530

    Add binary search problems

---
+++ binarysearch.com/linked_list_jumps.cpp
@@ -0,0 +1,28 @@
+// https://binarysearch.com/problems/Linked-List-Jumps
+
+/**
+ * LOGIC: Basic - just make the no. of jumps required and map the blocks
+ * COMPLEXITY: O(N)
+ */
+LLNode* solve(LLNode* node) {
+    
+    LLNode* save = node;
+    LLNode* saveHead = save;
+    
+    while(node) {
+        int counter = save->val;
+        while(counter--) {
+            if(!node) {
+                break;
+            }
+            node = node->next;
+        }
+        save->next = node;
+        save = node;
+    } 
+    if(save){
+        save->next = NULL;
+    }
+    
+    return saveHead;
+}
