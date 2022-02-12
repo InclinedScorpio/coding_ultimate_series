ea50e73f4a85ae14e2245635f00193d7ad2f22ea
Ashutosh Tiwari <ashutoshtiwari3309@gmail.com>
2022-02-12 14:24:57 +0530

    Add comments on rotate list

--- binarysearch.com/easy/questions/array/rotate_list_left_by_k.cpp
+++ binarysearch.com/easy/questions/array/rotate_list_left_by_k.cpp
@@ -1,5 +1,13 @@
 // https://binarysearch.com/problems/Rotate-List-Left-by-K
 
+/**
+ * LOGIC: Basic
+ * - We can use duplicate vector also
+ * - But here, partial vector is taken which will only store values upto k
+ * 
+ * TIMECOMPLEXITY: O(N)
+ * 
+ */ 
 vector<int> solve(vector<int>& nums, int k) {
     vector<int> partialVec;
     for(int i=0;i<k;i++) {
