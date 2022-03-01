dfc197e5d94f2d79d9e7e6da7595b8a7250a7e95
Ashutosh Tiwari <ashutoshtiwari3309@gmail.com>
2022-03-01 22:47:02 +0530

    ADD: Quick sort notes

--- algorithms/array/sorting/⭐️ quick_sort.cpp
+++ algorithms/array/sorting/⭐️ quick_sort.cpp
@@ -3,6 +3,16 @@
 
 using namespace std;
 
+/**
+ * QUICK_SORT
+ * LOGIC: partitioning algo is the most important part
+ * Partitioning algo - uses 2 ptr - i, j
+ * - 'i' will move right (if value is less than and equal to pivot{first})
+ * - 'j' will move left (if value is more than pivot{first})
+ * - Stop ptr when condition not satisfy and swap them
+ * - If 'j' >'i' then replace 'j' with pivot element so now pivot is at it's original position
+ * 
+ */ 
 int partition(int arr[], int l, int r) {
     int pivot = arr[l];
     int pivotIndex = l;
