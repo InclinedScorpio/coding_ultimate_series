47265a3b5d70cf0592238fa719e724010077e6c0
Ashutosh Tiwari <ashutoshtiwari3309@gmail.com>
2022-04-12 01:30:45 +0530

    Update upper, lower bound code

--- random_platform/misc/vector/upper_and_lower_bound.cpp
+++ random_platform/misc/vector/upper_and_lower_bound.cpp
@@ -10,46 +10,39 @@
 using namespace std;
 
 int vec_lower_bound(vector<int>&  arr, int num) {
-    int left=0, right=arr.size()-1;
+    int left=0, right=arr.size()-1, ans=-1;
     while(left<=right) {
         int mid = (left+right)/2;
         if(arr[mid]==num) {
-            if((mid!=0 && arr[mid-1]==num)) {
-                right =mid-1;
-            } else return mid;
+            ans=mid;
+            right=mid-1;
         } else if (arr[mid] > num ) {
             right=mid-1;
         } else {
             left=mid+1;
         }
     }
-    return -1;
+    return ans;
 }
 
 int vec_upper_bound(vector<int>& arr, int num) {
-    int left=0, right = arr.size()-1;
-
+    int left=0, right = arr.size()-1, ans=-1;
     while(left<=right) {
         int mid = (left+right)/2;
         if(arr[mid]==num) {
-            if(mid!=arr.size()-1 && arr[mid]==arr[mid+1]) {
-                left=mid+1;
-            } else if(mid!=arr.size()-1 && arr[mid]!=arr[mid+1]) {
-                return mid;
-            } else {
-                return mid;
-            }
+            ans = mid;
+            left=mid+1;
         } else if(arr[mid]<num) {
             left=mid+1;
         } else {
             right=mid-1;
         }
     }
-
+    return ans;
 }
 
 int main() {
     vector<int> arr = {1,1,2,2,2,2,3,5,77,888,999,999,999};
-    cout<<"lower_bound: "<<vec_lower_bound(arr, 999)<<endl;
-    cout<<"upper_bound: "<<vec_upper_bound(arr, 999);
+    cout<<"lower_bound: "<<vec_lower_bound(arr, 1)<<endl;
+    cout<<"upper_bound: "<<vec_upper_bound(arr, 1);
 }
