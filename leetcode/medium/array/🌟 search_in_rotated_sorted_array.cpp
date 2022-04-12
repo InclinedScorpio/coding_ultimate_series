a8f5a58bd883a8c3be7762c1bd987ec6cfbb696a
Ashutosh Tiwari <ashutoshtiwari3309@gmail.com>
2022-04-13 01:37:11 +0530

    Add another way of doing search in rotated sorted array problem

--- leetcode/medium/array/🌟 search_in_rotated_sorted_array.cpp
+++ leetcode/medium/array/🌟 search_in_rotated_sorted_array.cpp
@@ -1,6 +1,12 @@
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
 
 /**
+ * BETTERAPPROACH: No need to find the pivot element (use scratch!)
+ * - On mid, check if pivot lies on left, check if target lies on right (right is sorted)
+ * - If yes, start checking right, else check left!
+ * - Point is, on each time mid occurs, left or right would be sorted
+ * - Code at last!
+ * 
  * LOGIC:
  * Idea is to first check the point which is shifted!
  * - This can be done using Binary Search - O(logN)
@@ -58,6 +64,32 @@ public:
         }
         
         // no solution
+        return -1;
+    }
+};
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left=0, right=nums.size()-1;
+        while(left<=right) {
+            int mid = (left+right)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[left]>nums[mid]) {
+                // check right
+                if(nums[mid]<target && target<=nums[right]) {
+                    left=mid+1;
+                } else {
+                    right=mid-1;
+                }
+            } else {
+                if(nums[left]<=target && target<nums[mid]) {
+                    right=mid-1;
+                } else {
+                    left=mid+1;
+                }
+            }
+        }
         return -1;
     }
 };
