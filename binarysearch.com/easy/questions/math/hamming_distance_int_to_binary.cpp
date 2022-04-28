09205dba187e0d5a6f10a6e785ccde9e7b972912
Ashutosh Tiwari <ashutoshtiwari3309@gmail.com>
2022-04-28 23:50:30 +0530

    Add hamming distance problem

---
+++ binarysearch.com/easy/questions/math/hamming_distance_int_to_binary.cpp
@@ -0,0 +1,13 @@
+// https://binarysearch.com/problems/Hamming-Distance
+
+int solve(int x, int y) {
+    // char first[1000], second[1000];
+    string first = bitset<32>(x).to_string();
+    string second = bitset<32>(y).to_string();
+
+    int count=0;
+    for(int i=0;i<min(first.length(), second.length());i++) {
+        if(first[i]!=second[i]) count++;
+    }
+    return count;
+}
