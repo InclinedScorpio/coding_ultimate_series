e3f2fbd3ab4af6362938213eb75e345ae6966b45
Ashutosh Tiwari <ashutoshtiwari3309@gmail.com>
2022-05-16 02:29:04 +0530

    Add longest common subsequence problem

--- algorithms/dynamic programming/array/2D/🌟 longest_common_subsequence/🌟 2) iterative_bottom_up.cpp
+++ algorithms/dynamic programming/array/2D/🌟 longest_common_subsequence/🌟 2) iterative_bottom_up.cpp
@@ -11,9 +11,35 @@
 
 using namespace std;
 
+string findStringForming(string str1, string str2, vector<vector<int>> dp) {
+    int i=str1.length()-1, j=str2.length()-1;
+    string str = "";
+    while(i>0 && j>0) { // traverse back from end to top and see where we moved diagonally
+    // diagonal movement is only done logically when char matches hence store those characters!
+        if(str1[i]==str2[j]) {
+            str = str1[i] + str;
+            i--;j--;
+        } else {
+            if(dp[i-1][j]>=dp[i][j-1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+    }
+    if(i!=0) { // starting of i matches someone from j
+        str = str1[i] + str;
+    } else if(j!=0) { // starting of j matches someone from i
+        str = str2[j] + str;
+    } else { // if both i and j are 0 means 0th ind matches
+        str = str1[0] + str;
+    }
+    return str;
+}
+
 int main() {
-    string str1="ABCDXY";
-    string str2="ABEDGXY";
+    string str1="ABCDUiiO";
+    string str2="ZZZZABEDjjjjjUO";
     vector<vector<int>> dp(str1.length(), vector<int>(str2.length(), -1));
     for(int i=0;i<str2.length();i++) {
         if(str1[0]==str2[i]) {
@@ -34,13 +60,16 @@ int main() {
     for(int i=1;i<str1.length();i++) {
         for(int j=1;j<str2.length();j++) {
             if(str1[i]==str2[j]) {
-                if(dp[i][j-1]+1>dp[i-1][j]) {
-                    dp[i][j]=dp[i][j-1]+1;
-                    continue;
-                }
+                dp[i][j]=dp[i-1][j-1]+1;
+            } else {
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
             }
-            dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
         }
     }
     cout<<dp[str1.length()-1][str2.length()-1];
+    cout<<endl;
+    cout<<findStringForming(str1, str2, dp);
 }
+
+
+
