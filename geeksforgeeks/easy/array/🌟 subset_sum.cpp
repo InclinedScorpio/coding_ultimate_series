915fa3c0c9a68237f1ba4f1958d9954048f244a6
Ashutosh Tiwari <ashutoshtiwari3309@gmail.com>
2022-06-12 22:37:30 +0530

    Add subset sum problem

---
+++ geeksforgeeks/easy/array/🌟 subset_sum.cpp
@@ -0,0 +1,26 @@
+// https://practice.geeksforgeeks.org/problems/subset-sums2234/1#
+
+/**
+ * LOGIC: V.Imp to understand this way of calling recursively
+ * - Earlier I was using for loop which is not at all required
+ * - We can simply skip the current and take next one - that would also consider all subsets
+ * - Important to understand how we are generating all the subsets here
+ * 
+ * COMPLEXITY: O(2^N) - either we will consider or won't
+ * 
+ */ 
+public:
+    void helper(vector<int> arr, int N, int iter, int currSum, vector<int>& res) {
+        if(iter>=N) return;
+        res.push_back(currSum + arr[iter]);
+        helper(arr, N, iter+1, arr[iter] + currSum, res);
+        helper(arr, N, iter+1, currSum, res);
+        return;
+    }
+
+    vector<int> subsetSums(vector<int> arr, int N) {
+        vector<int> res = {0};
+        helper(arr, N, 0, 0, res);
+        return res;
+    }
+};
