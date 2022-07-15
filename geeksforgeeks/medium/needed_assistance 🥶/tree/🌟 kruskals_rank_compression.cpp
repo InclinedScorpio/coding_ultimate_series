177655e075293ec9ba92f8362560bc14e74bb85c
Ashutosh Tiwari <ashutoshtiwari3309@gmail.com>
2022-07-15 16:08:40 +0530

    Add disjoint set using rank and compression & kruskals algorithm problem

---
+++ geeksforgeeks/medium/needed_assistance 🥶/tree/🌟 kruskals_rank_compression.cpp
@@ -0,0 +1,62 @@
+// https://practice.geeksforgeeks.org/problems/minimum-spanning-tree/1/#
+
+/**
+ * LOGIC: Things to understand
+ * 1) Minimum spanning tree - V vertices,V-1 edges, sum of min edges
+ * 2) Keep picking minimum edges, but cycle may form
+ * 3) To avoid cycle make use of Disjoint sets
+ * 4) Rank and compression is optimized way of disjoint set O(4Alpha) ~ O(4)
+ * 
+ * 
+ * COMPLEXITY: O(NlogN) - cause of sorting
+ * 
+ */ 
+class Solution {
+	public:
+	
+	int parentOf(vector<int>& parent, int node) {
+	    if(parent[node]==-1) return node;
+	    return parent[node] = parentOf(parent, parent[node]);
+	}
+	
+	void nodeUnion(vector<int>& parent, vector<int>& rank, int first, int second) {
+	    int parentOfFirst = parentOf(parent, first);
+	    int parentOfSecond = parentOf(parent, second);
+	    if(parentOfFirst == parentOfSecond) return;
+	    if(rank[parentOfFirst]<rank[parentOfSecond]) {
+	        parent[parentOfFirst] = parentOfSecond;
+	    } else if(rank[parentOfFirst]>rank[parentOfSecond]) {
+	        parent[parentOfSecond] = parentOfFirst;
+	    } else {
+	        parent[parentOfFirst] = parentOfSecond;
+	        rank[parentOfSecond]++;
+	    }
+	}
+	
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        priority_queue<pair<int, pair<int, int>>,vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> maxHeap;
+        vector<int> isChoosen(V+1, 0);
+        for(int ind=0;ind<V;ind++) {
+            for(vector<int> i: adj[ind]) {
+                maxHeap.push({i[1], {ind, i[0]}});
+            }
+        }
+        
+        vector<int> parent(V, -1);
+        vector<int> rank(V, 0);
+        
+        int edgesRequired = V-1;
+        int sum=0;
+        while(edgesRequired) {
+            pair<int, pair<int, int>> top = maxHeap.top();
+            maxHeap.pop();
+            if(parentOf(parent, top.second.first)==parentOf(parent, top.second.second)) continue;
+            nodeUnion(parent, rank, top.second.first, top.second.second);
+            sum+=top.first;
+            edgesRequired--;
+        }
+        return sum;
+    }
+};
---
+++ random_platform/misc/disjoint_sets/⭐️ rank_and_compression_algorithm_disjoint_set.cpp
@@ -0,0 +1,101 @@
+/**
+ * QUESTION: To check if 2 nodes exists in same component or different component
+ * 
+ * LOGIC: We make use of parents and ranks for nodes.
+ * - Parent tell which is the parent of the node
+ * - We try to keep the height minimum (it's a tree basically)
+ * - While entering 2 nodes, we do union to show they are related
+ * - While doing union - ALWAYS PICK THE PARENT OF THOSE NODE TO UNION
+ * - Also the node which has less rank gets attached to larger rank (so rank doesn't increase)
+ * - Rank increases only when same rank parent are union (!important)
+ * 
+ * ## Compression
+ * - While doing find, if we see there are more steps to reach parent from a node, we do compression
+ * - Compression will just make sure that all child nodes directly point to parent rather than forming chain
+ * 
+ * 
+ * 
+ * 
+ * 
+ */ 
+
+#include<iostream>
+
+using namespace std;
+
+int parent[100000];
+int nodeRank[100000];
+
+void makeSet(int n) {
+    for(int i=0;i<n;i++) {
+        parent[i] = i;
+        nodeRank[i] = 0;
+    }
+}
+
+int findParent(int node) {
+    if(parent[node]==node) {
+        return node;
+    }
+    return parent[node]=findParent(parent[node]); //compression
+}
+
+void nodeUnion(int a, int b) {
+    int parentOfA = findParent(a);
+    int parentOfB = findParent(b);
+    if(parentOfA == parentOfB) return;
+    // always join smaller rank parent to larger rank parent! so rank remains the same (rank is basically depth!)
+    if(nodeRank[parentOfA]<nodeRank[parentOfB]) { // rank won't change if smaller rank is joined to larger rank
+        parent[parentOfA] = parentOfB;
+    } else if(nodeRank[parentOfA]>nodeRank[parentOfB]) { // rank won't change if smaller rank is joined to larger rank
+        parent[parentOfB] = parentOfA;
+    } else { //rank eq
+        parent[parentOfA] = parentOfB; 
+        nodeRank[parentOfB]++;  // increase rank, as both have same rank!
+    }
+}
+
+/**
+
+           1  --- 2 -- 3
+           |
+           4    
+
+        6 -- 7   8 -- 9
+                 |
+                 5
+ */ 
+
+
+int main() {
+    makeSet(10);
+    int connections;
+    cout<<"Enter total connections:";
+    cin>>connections;
+    cout<<endl;
+    for(int i=0;i<connections;i++) {
+        cout<<"Enter "<<i+1<<" connection from:";
+        int from, to;
+        cin>>from;
+        cout<<"Enter "<<i+1<<" connection to:";
+        cin>>to;
+        nodeUnion(from, to);
+        cout<<endl;
+    }
+
+    cout<<"Type 2 numbers to check if in same set: ";
+    int first, second;
+    cin>>first;
+    cin>>second;
+    while(first>=0 && first<10 && second>=0 && second<10) {
+        if(findParent(first)==findParent(second)) {
+            cout<<"same set";
+        } else {
+            cout<<"Different set";
+        }
+        cout<<endl;
+        cout<<"Type 2 numbers to check if in same set: ";
+        cin>>first;
+        cin>>second;
+    }
+}
