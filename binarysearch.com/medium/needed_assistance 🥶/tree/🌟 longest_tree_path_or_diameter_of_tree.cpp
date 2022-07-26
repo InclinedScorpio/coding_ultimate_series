fc9953e09163d3e765a6b363bedc372926f8ae24
Ashutosh Tiwari <ashutoshtiwari3309@gmail.com>
2022-07-26 12:26:05 +0530

    Add longest tree path or diameter of a tree problem

---
+++ binarysearch.com/medium/needed_assistance 🥶/tree/🌟 longest_tree_path_or_diameter_of_tree.cpp
@@ -0,0 +1,26 @@
+// https://binarysearch.com/problems/Longest-Tree-Path
+
+/**
+ * LOGIC: At each node we just need to find max left and right depth
+ * - But that doesn't have to be done at each step, rather make use of recursion
+ * - Recursion down to up here helps us to get depth without visited nodes multiple times
+ * 
+ * COMPLEXITY:O(N)
+ * 
+ * 
+ */ 
+int helper(Tree* root, int& result) {
+    if(!root) return 0;
+
+    int left = helper(root->left, result);
+    int right = helper(root->right, result);
+
+    result = max(left+right+1, result);
+    return 1 + max(left, right);
+}
+
+int solve(Tree* root) {
+    int result=0;
+    helper(root, result);
+    return result;
+}
