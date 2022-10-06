f1e99cdb9c5948af325eaa95eb459ca191db39a6
Ashutosh Tiwari <ashutoshtiwari3309@gmail.com>
2022-10-06 03:34:12 -0700

    Add search suggestion system problem

---
+++ leetcode/medium/trie/🌟 search_suggestions_system.cpp
@@ -0,0 +1,66 @@
+// https://leetcode.com/problems/search-suggestions-system/
+
+/**
+ * LOGIC: Basic Trie Problem! Must be done before to do it timely :)
+ * 
+ */ 
+class Trie {
+    public: 
+        Trie() {
+            memset(trie, NULL, 127);
+            isEnd=false;
+        }
+        Trie* trie[127] = {};
+        bool isEnd;
+        vector<string> store;
+};
+
+class Solution {
+public:
+    void insertIntoTrie(string str, Trie* paramCurrent) {
+        Trie* current = paramCurrent;
+        for(char i: str) {
+            if(!current->trie[i]) {
+                current->trie[i] = new Trie();
+            }
+            current = current->trie[i];
+            current->store.push_back(str);
+        }
+        current->isEnd = true;
+    }
+    
+    vector<string> search(string str, Trie* paramCurrent) {
+        Trie* current = paramCurrent;
+        for(char c: str) {
+            if(current->trie[c]) {
+                current = current->trie[c];
+            } else {
+                return {};
+            }
+        }
+        return current->store;
+    }
+    
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        Trie* myTrie = new Trie();
+        sort(products.begin(), products.end());
+        for(string str: products) {
+            insertIntoTrie(str, myTrie);
+        }
+        // check
+        string str="";
+        vector<vector<string>> result;
+        for(char i: searchWord) {
+            str+=i;
+            vector<string> res = search(str, myTrie);
+            int ind=0;
+            vector<string> temp;
+            while(ind<3 && ind<res.size()) {
+                temp.push_back(res[ind]);
+                ind++;
+            }
+            result.push_back(temp);
+        }
+        return result;
+    }
+};
